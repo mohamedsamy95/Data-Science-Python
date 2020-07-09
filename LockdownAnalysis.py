be83330fa2fa9ad7d6fde1cc9e8ed9f61629921c
Ashraf <beshtawi.ashraf@gmail.com>
2020-07-09 16:56:45 +0200

    Signed-off-by: Ashraf <beshtawi.ashraf@gmail.com>

--- LockdownAnalysis.py
+++ LockdownAnalysis.py
@@ -34,7 +34,7 @@ def plot_lockdown_flatten(hyprid_df):
     plt.xticks(rotation=25)
     sb.set_context("paper")
     sb.set_style("darkgrid")
-    sb.barplot(x="Country", y="Length",data=lockdown)
+    sb.barplot(x="Country", y="Lockdown Length",hue="Days to flatten the curve",data=hyprid_df)
     plt.title("The length of the lockdown")
     plt.show()
     return
@@ -42,7 +42,7 @@ def plot_lockdown_flatten(hyprid_df):
 
 
 #path of the project folder
-path="C:/Users/user/Desktop/COVID-19 Project"
+path="C:/Users/user/Desktop/Data-Science-Python"
 ##code...
 #change the working directory
 os.chdir(path)
@@ -58,7 +58,7 @@ print("Average length: "+str(avg))
 standard_deviation=np.std(lockdown["Length"])
 print("Standard deviation: "+str(standard_deviation))
 #plot lockdown
-#plot_lockdown(lockdown)
+plot_lockdown(lockdown)
 
 c0=covid19["Total Duration in Days"].to_list()
 c1=covid19["Country"].to_list()
@@ -83,10 +83,22 @@ for item in c2:
         sorted_length_to_flatten_curve.append(c0[indexof(c1,item)])
     else:
         sorted_length_to_flatten_curve.append(-1)
-print(sorted_length_to_flatten_curve)
-print(c2)
 
+##create new dataframe with 
+hy_df=pd.DataFrame(list(zip(c2,lockdown["Length"], sorted_length_to_flatten_curve)),columns=["Country","Lockdown Length","Days to flatten the curve"],index=c2)
 
+## plot the new data frame
+axes = hy_df.plot(kind='bar')
+plt.show()
+
+##calculate a factor
+## we calculate average of (number of infections and length of lockdown for each country)
+## the smaller the average the better the result of this country is
+
+##calculate the factor
+hy_df['lockdown_flatten_factor'] = hy_df.apply (lambda row:(row["Lockdown Length"]+row["Days to flatten the curve"])/2 , axis=1)
+
+print(hy_df)
         
 
 
@@ -97,6 +109,3 @@ print(c2)
 #onlyfiles = [f for f in os.listdir(economey_path) if os.path.isfile(os.path.join(economey_path, f))]
 #austeria=pd.read_csv(economey_path+onlyfiles[0])
 #print(austeria)
-
-
-
