72b3e881ee7f115e99cb22271ae42a6538528c9a
Ashraf <beshtawi.ashraf@gmail.com>
2020-07-09 22:32:39 +0200

    Signed-off-by: Ashraf <beshtawi.ashraf@gmail.com>

--- LockdownAnalysis.py
+++ LockdownAnalysis.py
@@ -4,6 +4,7 @@ import os
 import seaborn as sb
 from matplotlib import pyplot as plt
 import numpy as np
+import math
 ##Hilfsfunktionen
 def indexof(list,item):
     try:
@@ -63,12 +64,16 @@ plot_lockdown(lockdown)
 c0=covid19["Total Duration in Days"].to_list()
 c1=covid19["Country"].to_list()
 c2=lockdown["Country"].to_list()
+c4=covid19["firstExponential"].tolist() ## Date of the first exp-growth in infections
+c5=lockdown["Start of lockdown"].to_list() ## 
+
 ##countries with missing data
 not_found_country=[]
 
 for item in c1:
+    original_index=indexof(c1,item)
     index_value=indexof(c2,item)
-    if index_value<0:
+    if index_value<0 or math.isnan(c0[original_index]):
         not_found_country.append(item)
 ## print not found country
 print("Not found countries"+str(not_found_country))
@@ -85,8 +90,12 @@ for item in c2:
         sorted_length_to_flatten_curve.append(-1)
 
 ##create new dataframe with 
-hy_df=pd.DataFrame(list(zip(c2,lockdown["Length"], sorted_length_to_flatten_curve)),columns=["Country","Lockdown Length","Days to flatten the curve"],index=c2)
+hy_df=pd.DataFrame(list(zip(lockdown["Length"], sorted_length_to_flatten_curve)),columns=["Lockdown Length","Days to flatten the curve"],index=c2)
 
+## delete countries with missing data
+for land in  hy_df.iterrows():
+    if indexof(not_found_country,land[0])>0:
+        hy_df.drop([land[0]], inplace=True)
 ## plot the new data frame
 axes = hy_df.plot(kind='bar')
 plt.show()
@@ -96,12 +105,32 @@ plt.show()
 ## the smaller the average the better the result of this country is
 
 ##calculate the factor
-hy_df['lockdown_flatten_factor'] = hy_df.apply (lambda row:(row["Lockdown Length"]+row["Days to flatten the curve"])/2 , axis=1)
+#hy_df['lockdown_flatten_factor'] = hy_df.apply (lambda row:(row["Lockdown Length"]+row["Days to flatten the curve"])/2 , axis=1)
+
+
+## calculate the diffrence between the start of lockdown and the start of exp-growth
+dates=[]
+for land in  hy_df.iterrows():
+    index_covid=indexof(c1,land[0])
+    index_lockdown=indexof(c2,land[0])
+    ##error correction
+    ## if the country had no lockdown then drop
+    if(land[1]["Lockdown Length"]==0):
+        hy_df.drop([land[0]], inplace=True)
+    else:
+        dates.append(c5[index_lockdown]-c4[index_covid])
+## add dates to the dataframe
+hy_df["days_between_lockdown_first_exp_growth"]=dates
+hy_df["days_between_lockdown_first_exp_growth"]=hy_df["days_between_lockdown_first_exp_growth"].dt.days.astype('int16')
 
 print(hy_df)
-        
 
+axes = hy_df.plot(kind='bar')
+plt.show()
 
+###correlations
+x=hy_df["days_between_lockdown_first_exp_growth"].corr(hy_df["Lockdown Length"])
+print(x)
 
 
 ###read economical changes in the lockdown
